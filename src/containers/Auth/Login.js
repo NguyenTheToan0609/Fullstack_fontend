742846e2a04060d141205eeca9d1cd00df3f334b
NguyenTheToan0609 <thetoan0609@gmail.com>
2024-10-02 20:26:26 +0700

    Sử Dụng State và Cách Xử Lý Sự Kiện Event OnClick, OnChange với React

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -8,7 +8,35 @@ import { FormattedMessage } from "react-intl";
 class Login extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      username: "",
+      password: "",
+      isShowHide: false,
+    };
   }
+
+  handleOnchangeUsername = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+  };
+
+  handleOnchangePassword = (event) => {
+    this.setState({
+      password: event.target.value,
+    });
+  };
+
+  handleOnClick = () => {
+    alert("click me");
+  };
+
+  handleShowHidePassword = () => {
+    this.setState({
+      isShowHide: !this.state.isShowHide,
+    });
+  };
 
   render() {
     return (
@@ -22,18 +50,36 @@ class Login extends Component {
                 type="text"
                 className="form-control mt-1"
                 placeholder="Enter your username"
+                value={this.state.username}
+                onChange={(event) => this.handleOnchangeUsername(event)}
               />
             </div>
-            <div className="col-12 form-group">
+            <div className="col-12 form-group  input-login">
               <label>Password:</label>
-              <input
-                type="Password"
-                className="form-control  mt-1"
-                placeholder="Enter your password"
-              />
+              <div className="show-hide-password">
+                <input
+                  type={this.state.isShowHide ? "text" : "password"}
+                  className="form-control  mt-1"
+                  placeholder="Enter your password"
+                  value={this.state.password}
+                  onChange={(event) => this.handleOnchangePassword(event)}
+                />
+                <span onClick={() => this.handleShowHidePassword()}>
+                  <i
+                    class={
+                      this.state.isShowHide ? "far fa-eye" : "far fa-eye-slash"
+                    }
+                  ></i>
+                </span>
+              </div>
             </div>
             <div className="col-12">
-              <button className="btn-login">Login</button>
+              <button
+                className="btn-login"
+                onClick={() => this.handleOnClick()}
+              >
+                Login
+              </button>
             </div>
             <div className="col-12 mt-3 text-end">
               <span className="forgot-password"> Forgot your password?</span>
